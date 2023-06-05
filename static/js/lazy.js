b90d4048adcf830d4ee11f8b40e5cec4375322d7
Qianqian Wang <wangqianqian0608@zju.edu.cn>
2023-06-05 11:10:40 +0800

    test

--- static/js/lazy.js
+++ static/js/lazy.js
@@ -25,35 +25,16 @@
 // });
 
 
-  document.addEventListener("DOMContentLoaded", function () {
-    // Get all carousel video elements
-    var carouselVideos = document.querySelectorAll(".carousel-video");
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all carousel video elements
+  var carouselVideos = document.querySelectorAll(".carousel-video");
 
-    var options = {
-      root: null, // Use the viewport as the root element
-      rootMargin: "0px", // No margin applied to the root
-      threshold: 0.2, // Percentage of the video visible in the viewport
-    };
+  carouselVideos.forEach(function (video) {
+    // Get the video source URL from the data-src attribute
+    var videoSource = video.getAttribute("data-src");
 
-    // Create a new Intersection Observer
-    var observer = new IntersectionObserver(function (entries, observer) {
-      entries.forEach(function (entry) {
-        if (entry.isIntersecting) {
-          var video = entry.target;
-          var videoSource = video.getAttribute("data-src");
-
-          // Set the source URL of the video element
-          video.setAttribute("src", videoSource);
-          video.autoplay = true;
-
-          // Stop observing the video once it becomes visible
-          observer.unobserve(video);
-        }
-      });
-    }, options);
-
-    // Observe each carousel video element
-    carouselVideos.forEach(function (video) {
-      observer.observe(video);
-    });
+    // Set the source URL of the video element
+    video.setAttribute("src", videoSource);
+    video.autoplay = true;
   });
+});

0ea5c77c80f8393ecb6946a8eab1534e04baad38
Qianqian Wang <wangqianqian0608@zju.edu.cn>
2023-06-05 13:31:45 +0800

    fix carousel issue

--- static/js/lazy.js
+++ static/js/lazy.js
@@ -1,40 +1,33 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//   var lazyVideos = [].slice.call(document.querySelectorAll("video.lazy"));
-
-//   if ("IntersectionObserver" in window) {
-//     var lazyVideoObserver = new IntersectionObserver(function(entries, observer) {
-//       entries.forEach(function(video) {
-//         if (video.isIntersecting) {
-//           for (var source in video.target.children) {
-//             var videoSource = video.target.children[source];
-//             if (typeof videoSource.tagName === "string" && videoSource.tagName === "SOURCE") {
-//               videoSource.src = videoSource.dataset.src;
-//             }
-//           }
-
-//           video.target.load();
-//           video.target.classList.remove("lazy");
-//           lazyVideoObserver.unobserve(video.target);
-//         }
-//       });
-//     });
-//     lazyVideos.forEach(function(lazyVideo) {
-//       lazyVideoObserver.observe(lazyVideo);
-//     });
-//   }
-// });
-
-
 document.addEventListener("DOMContentLoaded", function () {
   // Get all carousel video elements
   var carouselVideos = document.querySelectorAll(".carousel-video");
 
-  carouselVideos.forEach(function (video) {
-    // Get the video source URL from the data-src attribute
-    var videoSource = video.getAttribute("data-src");
+  var options = {
+    root: null, // Use the viewport as the root element
+    rootMargin: "0px", // No margin applied to the root
+    threshold: 0.2, // Percentage of the video visible in the viewport
+  };
 
-    // Set the source URL of the video element
-    video.setAttribute("src", videoSource);
-    video.autoplay = true;
+  // Create a new Intersection Observer
+  var observer = new IntersectionObserver(function (entries, observer) {
+    console.log(observer)
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        var carouselVideos = document.querySelectorAll(".carousel-video");
+         carouselVideos.forEach(function (video) {
+          video.setAttribute("src", video.getAttribute("data-src"));
+          video.autoplay = true;
+        });
+
+        // Stop observing the video once it becomes visible
+        observer.disconnect();
+        observer.unobserve(video);
+      }
+    });
+  }, options);
+
+  // Observe each carousel video element
+  carouselVideos.forEach(function (video) {
+    observer.observe(video);
   });
 });
